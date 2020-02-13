5f2cfbe970460acd3f7c571a610f554423128006
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-02-13 18:00:25 +0100

    delete Todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,13 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
+
+//click on X to delete Todo
+$("span").click(function(event){
+    //fades out the List clicked on
+    $(this).parent().fadeOut(500, function(){
+        //removes the list entirely
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
