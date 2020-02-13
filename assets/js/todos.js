171438b894a9bfdcf1c5a58e259597b4a44cc5ed
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-02-13 18:37:16 +0100

    adds to do to ToDo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,26 @@
 //check off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function (event) {
     //fades out the List clicked on
     $(this).parent().fadeOut(500, function(){
         //removes the list entirely
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+//add Todo to Todo List
+$("input[type = 'text']").keypress(function (e) {
+    //if enter key is pressed
+    if (e.which === 13) {
+        //grab a new todo text from the input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
